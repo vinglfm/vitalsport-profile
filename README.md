9abb1db9b0c302f43b9af3e04c249530b439a47f
Andrew Pryshchepa <vinglfm@gmail.com>
2016-01-09 14:17:54 +0200

    Added Swagger support

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # vitalsport-profile
-#1 vitalsport-clubs;
-#2 Adderess API; - not a part of profile
-#3 vitalsport-search;
-#4 comment API; - possible different module;
-#5 vitalsport-content;
+API: http://localhost:8080/vitalsport/swagger-ui.html
--- src/main/java/com/vitalsport/profile/ProfileApplication.java
+++ src/main/java/com/vitalsport/profile/ProfileApplication.java
@@ -3,12 +3,36 @@ package com.vitalsport.profile;
 import com.vitalsport.profile.config.PersistenceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @SpringBootApplication(exclude = PersistenceConfiguration.class)
 public class ProfileApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class, args);
+    }
+
+    @Bean
+    public Docket profileApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("profile")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vitalsport-profile")
+                .description("Profile module API")
+                .version("0.9")
+                .build();
     }
 }
